e20af105f5d14792afd0ffa3ba7b2a93fc6a8510
Mario Campos <marcampo@c3a9c5.42berlin.de>
2025-03-26 19:42:10 +0100

    added basic REPL loop

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include "includes/minishell.h"
 
-int	main(int argc, char **argv, char **envp)
-{
-	return (0);
+// int	main(int argc, char **argv, char **envp)
+int main() {
+  char *line;
+
+  using_history();
+  while (1) {
+    line = readline("minishell> ");
+    if (!line) {
+      printf("\nExiting minishell\n");
+    }
+    add_history(line);
+    printf("Your line: %s\n", line);
+    free(line);
+  }
+  clear_history();
+  return (0);
 }
