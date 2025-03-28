a8026752189a2bc9bc6cb82e74ebc5f8f706412a
Mario Campos <marcampo@c3a2c2.42berlin.de>
2025-03-28 16:56:55 +0100

    pasted Giulias t_command struct declaration to minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -10,6 +10,17 @@
 #include <unistd.h>
 # include <fcntl.h>
 
+// TODO IMPORTANT Delete this before pull request
+typedef struct s_command {
+    char *name;               // Command name
+    char **arguments;            // Arguments
+    char *input_redirect;        // Input redirection file
+    char *output_redirect;       // Output redirection file
+    int append_output;           // Boolean for append mode >> or >
+    int is_pipe;                 // Boolean: Is this command part of a pipe?
+    struct s_command *next;      // Pointer to the next command in the sequence
+} t_command;
+
 // stuff copied directly from pipex
 char	*get_rawpath(char **envp);
 char	*get_cmd_path(char *cmd, char *rawpath);
