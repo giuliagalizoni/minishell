1cb06a9c6d9431ac260ad2ad53d131eb4b0a4d33
Mario Campos <marcampo@c3a2c2.42berlin.de>
2025-03-28 16:51:10 +0100

    basic executer functions pulled from pipex

---
+++ executer.c
@@ -0,0 +1,63 @@
+#include "includes/minishell.h"
+
+char	*get_rawpath(char **envp)
+{
+	char	*rawpath;
+	int		i;
+
+	i = 0;
+	rawpath = NULL;
+	while (*envp[i])
+	{
+		if (!ft_strncmp(envp[i], "PATH", 4))
+		{
+			rawpath = ft_strnstr(envp[i], "=", 5) + 1;
+			break ;
+		}
+		i++;
+	}
+	return (rawpath);
+}
+
+char	*get_cmd_path(char *cmd, char *rawpath)
+{
+	char	**paths;
+	char	*basepath;
+	char	*fullpath;
+	int		i;
+
+	i = -1;
+	paths = ft_split(rawpath, ':');
+	while (paths[++i])
+	{
+		basepath = ft_strjoin(paths[i], "/");
+		fullpath = ft_strjoin(basepath, cmd);
+		free(basepath);
+		basepath = NULL;
+		if (access(fullpath, X_OK) == 0)
+		{
+			free_array((void **)paths);
+			return (fullpath);
+		}
+		free(fullpath);
+		fullpath = NULL;
+	}
+	free(fullpath);
+	fullpath = NULL;
+	free_array((void **)paths);
+	return (NULL);
+}
+
+void	process(char **args, char **envp)
+{
+	pid_t	pid;
+	pid = fork();
+	if (pid == 0)
+	{
+		cmd = get_cmd_path(args[0], get_rawpath(envp)
+	}
+	if (cmd)
+		execve(cmd, &args[0], envp);
+	free(cmd);
+	perror("Command not found");
+}
