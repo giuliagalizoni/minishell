b01e5b472538a52d2c1be706fbaa9a04030ccd9c
Mario Campos <marcampo@c3a10c5.42berlin.de>
2025-04-01 15:19:20 +0200

    moved util functions to their util files

--- command_utils.c
+++ command_utils.c
@@ -2,3 +2,16 @@
 
 // TODO delete command lists adapted from list delete fns
 // move command_init here
+void	command_init(t_command *command)
+{
+	command->name = NULL;
+	command->arguments = NULL;
+	command->input_redirect = NULL;
+	command->is_heredoc = 0;
+	command->heredoc_delimiter = NULL;
+	command->output_redirect = NULL;
+	command->append_output = 0;
+	command->is_pipe = 0;
+	command->next = NULL;
+	command->pipe_next = NULL;
+}
