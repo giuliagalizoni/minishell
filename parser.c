f0027311bc7f653fda3a618d9659c0b4f3d695e5
Giulia <96666886+giuliagalizoni@users.noreply.github.com>
2025-04-01 16:02:07 +0200

    refactored analyser function to correct segfault (#12)
    Co-authored-by: Giulia Galizoni Caversan <ggalizon@c3a10c6.42berlin.de>

--- parser.c
+++ parser.c
@@ -16,6 +16,7 @@ char	**lexer(char *line)
 t_command	analyser(char **tokens)
 {
 	t_command command;
+	int i;
 
 	command_init(&command);
 	//idk yet if this rule works for all the cases
@@ -23,37 +24,38 @@ t_command	analyser(char **tokens)
 		command.name = ft_strdup(tokens[0]);
 	else
 		command.name = ft_strdup(tokens[2]);
-	while (*tokens) //I'm not completely sure if the use of strncmp is the best one for all cases, but for now it's working
+	i = 0;
+	while (tokens[i]) //I'm not completely sure if the use of strncmp is the best one for all cases, but for now it's working
 	{
-		if (!ft_strncmp(*tokens, "<<", 2)) //not heredoc, regular input redirection
+		if (!ft_strncmp(tokens[i], "<<", 2)) //not heredoc, regular input redirection
 			command.is_heredoc = 1;
-		else if (!ft_strncmp(*tokens, "<", 1)) //heredoc
+		else if (!ft_strncmp(tokens[i], "<", 1)) //heredoc
 			command.is_heredoc = 0;
-		else if (*(tokens-1) && !ft_strncmp(*(tokens-1), "<", 1)) // if the previous is redirection operator, the current will be infile
-			command.input_redirect = ft_strdup(*tokens);
-		else if (*(tokens-1) && !ft_strncmp(*(tokens-1), "<<", 2)) // if the previous is heredoc operator, the current will be heredoc delimiter
-			command.heredoc_delimiter = ft_strdup(*tokens);
-		else if (!ft_strncmp(*tokens, ">>", 2)) // just redirect, not append
+		else if (i > 0 && !ft_strncmp(tokens[i-1], "<", 1)) // if the previous is redirection operator, the current will be infile
+			command.input_redirect = ft_strdup(tokens[i]);
+		else if (i > 0 && !ft_strncmp(tokens[i-1], "<<", 2)) // if the previous is heredoc operator, the current will be heredoc delimiter
+			command.heredoc_delimiter = ft_strdup(tokens[i]);
+		else if (!ft_strncmp(tokens[i], ">>", 2)) // just redirect, not append
 			command.append_output = 1;
-		else if (!ft_strncmp(*tokens, ">", 1)) // append
+		else if (!ft_strncmp(tokens[i], ">", 1)) // append
 			command.append_output = 0;
-		else if ((*tokens -1) && !ft_strncmp((*tokens-1), ">>", 2))
-			command.output_redirect = ft_strdup(*tokens);
-		else if (*(tokens-1) && !ft_strncmp(*(tokens-1), ">", 1))
-			command.output_redirect = ft_strdup(*tokens);
-		else if (!ft_strncmp(*tokens, "|", 1)) // pipe
+		else if ((tokens[i] -1) && !ft_strncmp((tokens[i]-1), ">>", 2))
+			command.output_redirect = ft_strdup(tokens[i]);
+		else if (i > 0 && !ft_strncmp(tokens[i-1], ">", 1))
+			command.output_redirect = ft_strdup(tokens[i]);
+		else if (!ft_strncmp(tokens[i], "|", 1)) // pipe
 		{
 			command.is_pipe = 1;
 			tokens++;
 			command.pipe_next = malloc(sizeof(t_command));
 			if (!command.pipe_next)
 				return command;
-			*command.pipe_next = analyser(tokens);
+			*command.pipe_next = analyser(tokens + (i++));
 			break ;
 		}
 		else // all other cases will be arguments
-			arr_push(&command.arguments, *tokens);
-		tokens++;
+			arr_push(&command.arguments, tokens[i]);
+		i++;
 	}
 	return (command);
 }
@@ -69,7 +71,7 @@ void	parser(char *line, t_command *command)
 	*command = analyser(tokens);
 	free_arr((void **)tokens);
 }
-
+// parser testing main
 // int	main()
 // {
 // 	char *line = "< infile grep a1 | wc -w >> outfile";
