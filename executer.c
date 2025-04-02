bc0950e6132c1b9e2eb3439e58cee68c15de57a4
Mario Campos <marcampo@c1b1c1.42berlin.de>
2025-04-02 17:05:11 +0200

    executer WIP

--- executer.c
+++ executer.c
@@ -10,3 +10,29 @@ void	process(t_command *cmd)
 	}
 	wait(NULL);
 }
+
+void	process(t_command *cmd)
+{
+	while (cmd)
+	{
+		child_process(cmd);
+		cmd = cmd->next;
+	}
+}
+
+void	child_process(t_command *cmd)
+{
+	pid_t	pid;
+	int	fd[2];
+	
+	// we need to
+	// check that it's the first or last cmd in the pipe
+	// otherwise we redirect it's stdin to the read end of the last cmd
+	// and or the 
+	if (pipe(fd) == -1)
+		perror("Pipe creation error");// wrapper fn for cleanup?
+	pid = fork();
+	if (pid == -1)
+		perror("Fork creation error");
+	if (pid == -1)
+}
