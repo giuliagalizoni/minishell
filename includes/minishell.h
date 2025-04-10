8ef58dd50499a2993c47e3e0ce461beb14d1a689
Giulia Galizoni Caversan <ggalizon@c3a10c2.42berlin.de>
2025-04-10 17:31:04 +0200

    changes to fix malloc error broken for now

--- includes/minishell.h
+++ includes/minishell.h
@@ -45,7 +45,7 @@ void	parent_process(t_command *cmd, pid_t *pids, int pid, int *fd, int *prev_pip
 void	process(t_command *cmd, int num_cmds);
 // parser
 void	parser(char *line, t_command *command, char **envp);
-t_command	analyser(char **tokens, int index, char **envp);
+t_command	*analyser(char **tokens, int index, char **envp);
 char	**lexer(char *line, char ***tokens);
 // builtin_utils
 int	is_builtin(char *name);
