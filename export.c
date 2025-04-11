774415db545cab9e5345b8f00d12cb65a8abb975
Giulia Galizoni Caversan <ggalizon@c3a10c4.42berlin.de>
2025-04-11 16:46:51 +0200

    notes for export

---
+++ export.c
@@ -0,0 +1,17 @@
+#include "includes/minishell.h"
+
+void	export()
+{
+	// array of export variables
+	// copy of envp
+
+	// key=value
+	// if key -> update
+	// if !key -> add key and value
+	// if !value -> keep key empty
+	// add key to envp
+
+	//export alone should show all variables
+
+	//syntax errors
+}
