3e7d6cbf6886fbbfda5bb709b05c10c4ead3decd
Giulia Galizoni Caversan <ggalizon@c3a10c4.42berlin.de>
2025-04-11 13:36:38 +0200

    created echo function

---
+++ echo.c
@@ -0,0 +1,28 @@
+#include "includes/minishell.h"
+
+void	echo(t_command *cmd)
+{
+	int	i;
+	int flag = 0;
+
+	if (!cmd->arguments[1])
+	{
+		printf("\n");
+		return ;
+	}
+	i = 1;
+	if (is_equal(cmd->arguments[1], "-n"))
+	{
+		i++;
+		flag = 1;
+	}
+	while (cmd->arguments[i])
+	{
+		printf("%s", cmd->arguments[i]);
+		if (cmd->arguments[i + 1])
+			printf(" ");
+		i++;
+	}
+	if (!flag)
+		printf("\n");
+}
