87e04c753dee450e14c2c9a36d9deac1e6a5a548
Mario Campos <marcampo@c1b3c1.42berlin.de>
2025-04-13 17:21:49 +0200

    moved .c files to src, updated makefile

--- src/includes/minishell.h
+++ src/includes/minishell.h
@@ -1,6 +1,6 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
-# include "../libft/src/libft.h"
+# include "../../libft/src/libft.h"
 # include <fcntl.h>
 # include <readline/history.h>
 # include <readline/readline.h>
