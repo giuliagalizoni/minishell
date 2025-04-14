95647fa3677f536d6e5f833662328e57aceebcf4
Giulia Galizoni <giuliagalizoni@gmail.com>
2025-04-14 12:11:42 +0200

    moved export.c to src and cleaned printfs

--- src/export.c
+++ src/export.c
@@ -4,7 +4,6 @@ void	print_arr(char **arr)
 {
 	int	i;
 
-	printf("printer function\n");
 	i = 0;
 	while(arr[i])
 	{
@@ -17,13 +16,10 @@ void	export(t_command *cmd, char **envp)
 	// char **envp_copy;
 	// char **exports;
 
-	printf("calling executer\n");
 	// envp_copy = envp;
 	if (!cmd->arguments[1])
-	{
-		printf("no arguments\n");
 		print_arr(envp);
-	}
+
 
 	// array of export variables
 	// copy of envp
