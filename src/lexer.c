520cce3ec70b694472b687d9050268f944ee1cd0
Giulia Galizoni Caversan <ggalizon@c3a10c2.42berlin.de>
2025-04-15 17:15:24 +0200

    norminette friendly refactor

--- src/lexer.c
+++ src/lexer.c
@@ -1,16 +1,5 @@
 #include "includes/minishell.h"
 
-// static void	push_reset(char ***tokens, char *line, int *start, int *len)
-// {
-// 	char	*token;
-
-// 	token = ft_substr(line, *start, *len);
-// 	arr_push(tokens, token);
-// 	free(token);
-// 	*start = 0;
-// 	*len = 0;
-// }
-
 //this could be later moved to another file
 char *ft_strncat(char *dest, const char *src, size_t n)
 {
@@ -58,27 +47,6 @@ static void	handle_quotes(char *line, int *i, char *result)
 	ft_strncat(result, &line[start], *i-start);
 	(*i)++;
 }
-
-// static void	handle_operators(char *line, char ***tokens, int *i, int *start, int *len)
-// {
-// 	*start = *i;
-// 	if ((line[*i] == '<' && line[(*i) + 1] == '<') || (line[*i] == '>' && line[(*i) + 1] == '>'))
-// 		(*i)++;
-// 	*len = (*i - *start) + 1;
-// 	push_reset(tokens, line, start, len);
-// 	(*i)++;
-// }
-
-// static void	handle_tokens(char *line, char ***tokens, int *i, int *start, int *len)
-// {
-// 	*start = *i;
-// 	while (line[*i] && line[*i] != ' ' && line[*i] != '|' && line[*i] != '<'
-// 		&& line[*i] != '>' && line[*i] != 34 && line[*i] != 39)
-// 		(*i)++;
-// 	*len = *i - *start;
-// 	push_reset(tokens, line, start, len);
-// }
-
 static void	handle_combined_token(char *line, int *i, char *result)
 {
 	int	start;
@@ -90,18 +58,36 @@ static void	handle_combined_token(char *line, int *i, char *result)
 	ft_strncat(result, &line[start], *i-start);
 }
 
+static void	handle_operators(char *line, char ***tokens, char *result, int *i)
+{
+	push_token(tokens, result);
+	result[0] = line[*i];
+	if ((line[*i] == '<' && line[*i + 1] == '<') || (line[*i] == '>' && line[*i + 1] == '>'))
+	{
+		result[1] = line[(*i)++];
+		result[2] = '\0';
+	}
+	push_token(tokens, result);
+	(*i)++;
+}
+
+static void	init_buff(char *line, char **result)
+{
+	*result = (char *)malloc((ft_strlen(line) + 1) * sizeof(char));
+	if (!*result)
+	{
+		perror("malloc failed");
+		return ;
+	}
+	*result[0] = '\0';
+}
+
 char	**lexer(char *line, char ***tokens)
 {
 	int i;
 	char *result;
 
-	result = (char *)malloc((ft_strlen(line) + 1) * sizeof(char));
-	if (!result)
-	{
-		perror("malloc failed");
-		return NULL;
-	}
-	result[0] = '\0';
+	init_buff(line, &result);
 	i = 0;
 	while(line[i])
 	{
@@ -113,17 +99,7 @@ char	**lexer(char *line, char ***tokens)
 		if (line[i] == 34 || line[i] == 39)
 			handle_quotes(line, &i, result);
 		else if (line[i] == '|' || line[i] == '<' || line[i] == '>')
-		{
-			push_token(tokens, result);
-			result[0] = line[i];
-			if ((line[i] == '<' && line[i + 1] == '<') || (line[i] == '>' && line[i + 1] == '>'))
-			{
-				result[1] = line[i++];
-				result[2] = '\0';
-			}
-			push_token(tokens, result);
-			i++;
-		}
+			handle_operators(line, tokens, result, &i);
 		else
 			handle_combined_token(line, &i, result);
 	}
@@ -131,27 +107,3 @@ char	**lexer(char *line, char ***tokens)
 	free(result);
 	return *tokens;
 }
-// char **lexer(char *line, char ***tokens)
-// {
-// 	int i;
-// 	int start;
-// 	int len;
-
-// 	i = 0;
-// 	start = 0;
-// 	len = 0;
-// 	while(line[i])
-// 	{
-// 		while (line[i] && (line[i] == 32 || (line[i] >= 9 && line[i] <= 13)))
-// 			i++;
-// 		if (!line[i])
-// 			break ;
-// 		if (line[i] == 39 || line[i] == 34)
-// 			handle_quotes(line, tokens, &i, &start, &len);
-// 		else if (line[i] == '|' || line[i] == '<' || line[i] == '>')
-// 			handle_operators(line, tokens, &i, &start, &len);
-// 		else
-// 			handle_tokens(line, tokens, &i, &start, &len);
-// 	}
-// 	return *tokens;
-// }
