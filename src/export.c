28f2b55b86d9f11b3bf311b3d5c737e3c794f3ee
Giulia <96666886+giuliagalizoni@users.noreply.github.com>
2025-04-20 18:01:56 +0200

    Export (#52)
    export with key syntax check working
    
    ---------
    
    Co-authored-by: Giulia Galizoni Caversan <ggalizon@c3a10c2.42berlin.de>

--- src/export.c
+++ src/export.c
@@ -1,7 +1,6 @@
 #include "includes/minishell.h"
 
 // TODO
-// order list by ascii
 // deal with syntax errors *= bash: export: `*=': not a valid identifier
 // cleanup before exiting
 t_vars	*find_last(t_vars *lst)
@@ -34,9 +33,51 @@ void	print_vars(t_vars *vars)
 	{
 		if (!vars->key)
 			return ;
-		printf("export %s=\"%s\"\n", vars->key, vars->value);
+		if (vars->value)
+			printf("export %s=\"%s\"\n", vars->key, vars->value);
+		else
+		printf("export %s\n", vars->key);
 		vars = vars->next;
 	}
+}
+int	validate_key(char	*key)
+{
+	int i;
+
+	i = 0;
+	if (!ft_isalpha(key[i]) && !(key[i] == '_'))
+		return 1;
+	while (key[i])
+	{
+		if (!ft_isalnum(key[i]) && !(key[i] == '_'))
+			return 1;
+		i++;
+	}
+	return 0;
+}
+
+void	init_value(char **key, char **value, const char *arg, char *eq)
+{
+	char *temp;
+
+	*key = ft_substr(arg, 0, eq - arg);
+	*value = ft_strdup(eq + 1);
+	if (*value && (*value[0] == '\'' || *value[0] == '\"') && *value[ft_strlen(*value) - 1] == *value[0])
+	{
+		temp = ft_substr(*value, 1, ft_strlen(*value) - 2);
+		free(*value);
+		*value = temp;
+	}
+}
+
+void *handle_malloc_error(char **key, char **value, char *str)
+{
+	if (key)
+		free(key);
+	if (value)
+		free(value);
+	perror(str);
+	return (NULL);
 }
 
 t_vars *parse_var(const char *arg)
@@ -51,27 +92,21 @@ t_vars *parse_var(const char *arg)
 	{
 		key = ft_strdup(arg);
 		value = NULL;
-    }
-    else
-    {
-        key = ft_substr(arg, 0, eq - arg);
-        value = ft_strdup(eq + 1);
-        // if value == ""
-		//		value = next argument on cmd->arguments array
-		if (value && (value[0] == '\'' || value[0] == '\"') && value[ft_strlen(value) - 1] == value[0])
-        {
-            char *temp = ft_substr(value, 1, ft_strlen(value) - 2);
-            free(value);
-            value = temp;
-        }
-    }
-
-    new_var = malloc(sizeof(t_vars));
-    new_var->key = key;
-    new_var->value = value;
-    new_var->next = NULL;
-
-    return new_var;
+	}
+	else
+		init_value(&key, &value, arg, eq);
+	if (validate_key(key))
+	{
+		printf("conchinha: export: `%s\': not a valid identifier\n", arg); // check error code thingy
+		return (NULL);
+	}
+	new_var = malloc(sizeof(t_vars));
+	if (!new_var)
+		return handle_malloc_error(&key, &value, "malloc failed in parse_var");
+	new_var->key = key;
+	new_var->value = value;
+	new_var->next = NULL;
+	return new_var;
 }
 
 t_vars *init_envp(char **envp)
@@ -83,7 +118,8 @@ t_vars *init_envp(char **envp)
 	while (envp[i])
 	{
 		new_var = parse_var(envp[i]);
-		push_list(&head, new_var);
+		if (new_var)
+			push_list(&head, new_var);
 		i++;
 	}
 	return head;
@@ -91,25 +127,87 @@ t_vars *init_envp(char **envp)
 
 void	add_or_update_var(t_vars **head, char *key, char *value)
 {
-	t_vars *current = *head;
+	t_vars *current;
 	t_vars *new_var;
 
+	current = *head;
 	while(current)
 	{
 		if (is_equal(current->key, key))
 		{
-			free(current->value);
-			current->value = ft_strdup(value);
+			free(current->value); // maybe I need to init ft_strdup in a variable so I can free // it's not working so I'll leave it for later
+			if (value)
+				current->value = ft_strdup(value);
+			else
+				current->value = NULL;
 			return;
 		}
 		current = current->next;
 	}
-
 	new_var = malloc(sizeof(t_vars));
 	new_var->key = ft_strdup(key);
-	new_var->value = ft_strdup(value);
+	if (value)
+		new_var->value = ft_strdup(value);
+	else
+		new_var->value = NULL;// TODO: Check ft_strdup results
 	new_var->next = NULL;
 	push_list(head, new_var);
+}
+
+static void	swap_vars(t_vars *a, t_vars *b)
+{
+	char	*temp_key;
+	char	*temp_value;
+
+	temp_key = a->key;
+	temp_value = a->value;
+	a->key = b->key;
+	a->value = b->value;
+	b->key = temp_key;
+	b->value = temp_value;
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] > s2[i])
+			return (1);
+		else if (s1[i] < s2[i])
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+
+void	sort_vars_list(t_vars *head)
+{
+	int		swapped;
+	t_vars	*current;
+	t_vars	*last_ptr;
+
+	last_ptr = NULL;
+	if (head == NULL || head->next == NULL)
+		return ;
+	swapped = 1;
+	while (swapped)
+	{
+		swapped = 0;
+		current = head;
+		while (current->next != last_ptr)
+		{
+			if (ft_strcmp(current->key, current->next->key) > 0)
+			{
+				swap_vars(current, current->next);
+				swapped = 1;
+			}
+			current = current->next;
+		}
+		last_ptr = current;
+	}
 }
 
 void	export(t_msh *msh)
@@ -119,6 +217,7 @@ void	export(t_msh *msh)
 
 	if (!msh->command->arguments[1])
 	{
+		sort_vars_list(msh->myenv);
 		print_vars(msh->myenv);
 		return ;
 	}
