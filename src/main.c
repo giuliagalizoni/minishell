b24952c551fb1d4cacdcbbfd417e87d7a9848a44
Mario Campos <marcampo@c1b3c1.42berlin.de>
2025-04-23 17:29:02 +0200

    removed test main and added the little space in front of the prompt

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@ int	main(int argc, char **argv, char **envp) {
 	print_banner();
 	while (1)
 	{
-		line = readline("\033[38;5;199mconchinha\033[38;5;99m>\033[0m");
+		line = readline("\033[38;5;199mconchinha\033[38;5;99m>\033[0m ");
 		msh.command = parser(line, &msh, envp);
 		// set_command_paths(command, envp);
 		msh.num_cmds = count_commands(msh.command);
@@ -29,21 +29,3 @@ int	main(int argc, char **argv, char **envp) {
 	clear_history();
 	return (0);
 }
-
-/*
-void	output_test()
-{
-	int	file;
-
-	file = open("testytest", O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	dup2(file, 1);
-	close(file);
-}
-
-int	main()
-{
-	output_test();
-	printf("testy\n");
-	return (0);
-}
-*/
