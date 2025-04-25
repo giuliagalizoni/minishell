969c31d91e4eb5c0a4707b9f7c73b32339c1215a
Mario Campos <marcampo@c1b4c1.42berlin.de>
2025-04-25 22:22:58 +0200

    signals wip

--- src/main.c
+++ src/main.c
@@ -1,11 +1,34 @@
 #include "includes/minishell.h"
 
+
+void	INTHandler(int sig)
+{
+	signal(sig, SIG_IGN);
+	readline("\033[38;5;199mconchinha\033[38;5;99m>\033[0m ");
+}
+
+void	sigint_handler(int signal)
+{
+	if (signal == SIGINT)
+		printf("\nsiguinting!\n");
+}
+
+void	set_signal_action(void)
+{
+	struct sigaction act;
+	bzero(&act, sizeof(act));
+	act.sa_handler = &sigint_handler;
+	sigaction(SIGINT, &act, NULL);
+}
+
 int	main(int argc, char **argv, char **envp) {
-	char	*line;
+//	char	*line;
 	t_msh	msh;
 
 	(void)argc;
 	(void)argv;
+
+	set_signal_action();
 	msh.command = NULL;
 	msh.myenv = init_envp(envp);
 	msh.exit_status = 0;
@@ -13,6 +36,8 @@ int	main(int argc, char **argv, char **envp) {
 	print_banner();
 	while (1)
 	{
+		/*
+		signal(SIGINT, INTHandler);
 		line = readline("\033[38;5;199mconchinha\033[38;5;99m>\033[0m ");
 		msh.command = parser(line, &msh, envp);
 		// set_command_paths(command, envp);
@@ -23,6 +48,7 @@ int	main(int argc, char **argv, char **envp) {
 		      printf("\nExiting minishell\n");
 		add_history(line);
 		free(line);
+		*/
 	}
 	// free(command);
 	clear_history();
