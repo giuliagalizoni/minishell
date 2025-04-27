88ec8ee0ca8c04afecfdadbf69c83cd8f21b49e1
Giulia Galizoni <giuliagalizoni@gmail.com>
2025-04-27 20:20:33 +0200

    add functions to check syntax error in the but still need refactor

--- src/parser.c
+++ src/parser.c
@@ -80,6 +80,65 @@ t_command	*analyser(char **tokens, int index, char **envp, t_msh *msh)
 	return (command);
 }
 
+int is_redirection(char *token) {
+	if (!token) return 0;
+	return (is_equal(token, ">") || is_equal(token, "<") ||
+			is_equal(token, ">>") || is_equal(token, "<<"));
+}
+
+int is_pipe(char *token) {
+	if (!token) return 0;
+	return (is_equal(token, "|"));
+}
+
+int is_operator(char *token) {
+	if (!token) return 0; // Handle NULL token
+	return (is_pipe(token) || is_redirection(token));
+}
+// maybe change printfs to write or code a special printf to print in stderr
+static int check_invalid_syntax(char **tokens)
+{
+	int i;
+	int last;
+
+	if (!tokens || !tokens[0])
+		return (1);
+	if (is_operator(tokens[0]) && is_pipe(tokens[0]))
+	{
+		printf("minishell: syntax error near unexpected token `%s'\n", tokens[0]);
+		return (0);
+	}
+	last = -1;
+	while (tokens[last + 1])
+		last++;
+	if (last >= 0 && is_operator(tokens[last]))
+	{
+		printf("minishell: syntax error near unexpected token `newline'\n");
+		return (0);
+	}
+	i = 0;
+	while (tokens[i])
+	{
+		if (is_redirection(tokens[i]))
+		{
+			if (!tokens[i+1] || is_operator(tokens[i+1]))
+			{
+				printf("minishell: syntax error near unexpected token `%s'\n", tokens[i]);
+				return (0);
+			}
+		}
+		else if (is_pipe(tokens[i]))
+		{
+			if (!tokens[i+1] || is_pipe(tokens[i+1]))
+			{
+				printf("minishell: syntax error near unexpected token `%s'\n", tokens[i]);
+				return (1);
+			}
+		}
+		i++;
+	}
+	return (1);
+}
 
 t_command	*parser(char *line, t_msh *msh, char **envp)
 {
@@ -91,8 +150,14 @@ t_command	*parser(char *line, t_msh *msh, char **envp)
 	tokens = NULL;
 	tokens = lexer(line, &tokens);
 	// should the parser return an int so we can check the status code?
+	//syntax error code 2
 	if (!tokens)
 		return NULL;
+	if (tokens && !check_invalid_syntax(tokens))
+	{
+		free_arr((void **)tokens);
+		return (NULL);
+	}
 	retokens = expand_and_retokenize(tokens, msh);
 	msh->command = analyser(retokens, 0, envp, msh);
 	free_arr((void **)tokens);
