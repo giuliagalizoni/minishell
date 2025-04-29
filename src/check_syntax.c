287fbeae8e4de6ed5d490116b237fe2298867aa8
Giulia Galizoni <giuliagalizoni@gmail.com>
2025-04-29 16:30:34 +0200

    organizing and separationg functions

--- src/check_syntax.c
+++ src/check_syntax.c
@@ -1,19 +1,37 @@
 #include "includes/minishell.h"
 
 static int is_redirection(char *token) {
-	if (!token) return 0;
+	if (!token)
+		return (0);
 	return (is_equal(token, ">") || is_equal(token, "<") ||
 			is_equal(token, ">>") || is_equal(token, "<<"));
 }
 
 static int is_pipe(char *token) {
-	if (!token) return 0;
+	if (!token)
+		return (0);
 	return (is_equal(token, "|"));
 }
 
 static int is_operator(char *token) {
-	if (!token) return 0; // Handle NULL token
+	if (!token)
+		return (0);
 	return (is_pipe(token) || is_redirection(token));
+}
+
+static int tokens_check(char **tokens, int i)
+{
+	if (is_redirection(tokens[i]))
+		{
+			if (!tokens[i+1] || is_operator(tokens[i+1]))
+				return (p_syntax_error(tokens[i+1]));
+		}
+	else if (is_pipe(tokens[i]))
+	{
+		if (!tokens[i+1] || is_pipe(tokens[i+1]))
+			return (p_syntax_error(tokens[i+1]));
+	}
+	return (1);
 }
 
 int check_invalid_syntax(char **tokens)
@@ -33,16 +51,8 @@ int check_invalid_syntax(char **tokens)
 	i = 0;
 	while (tokens[i])
 	{
-		if (is_redirection(tokens[i]))
-		{
-			if (!tokens[i+1] || is_operator(tokens[i+1]))
-				return (p_syntax_error(tokens[i+1]));
-		}
-		else if (is_pipe(tokens[i]))
-		{
-			if (!tokens[i+1] || is_pipe(tokens[i+1]))
-				return (p_syntax_error(tokens[i+1]));
-		}
+		if (!tokens_check(tokens, i))
+			return (0);
 		i++;
 	}
 	return (1);
--- src/parser.c
+++ src/parser.c
@@ -67,54 +67,6 @@ t_command	*analyser(char **tokens, int index, char **envp, t_msh *msh)
 	return (command);
 }
 
-// int is_redirection(char *token) {
-// 	if (!token) return 0;
-// 	return (is_equal(token, ">") || is_equal(token, "<") ||
-// 			is_equal(token, ">>") || is_equal(token, "<<"));
-// }
-
-// int is_pipe(char *token) {
-// 	if (!token) return 0;
-// 	return (is_equal(token, "|"));
-// }
-
-// int is_operator(char *token) {
-// 	if (!token) return 0; // Handle NULL token
-// 	return (is_pipe(token) || is_redirection(token));
-// }
-
-// static int check_invalid_syntax(char **tokens)
-// {
-// 	int i;
-// 	int last;
-
-// 	if (!tokens || !tokens[0])
-// 		return (1);
-// 	if (is_operator(tokens[0]) && is_pipe(tokens[0]))
-// 		return (p_syntax_error(tokens[0]));
-// 	last = -1;
-// 	while (tokens[last + 1])
-// 		last++;
-// 	if (last >= 0 && is_operator(tokens[last]))
-// 		return (p_syntax_error(NULL));
-// 	i = 0;
-// 	while (tokens[i])
-// 	{
-// 		if (is_redirection(tokens[i]))
-// 		{
-// 			if (!tokens[i+1] || is_operator(tokens[i+1]))
-// 				return (p_syntax_error(tokens[i+1]));
-// 		}
-// 		else if (is_pipe(tokens[i]))
-// 		{
-// 			if (!tokens[i+1] || is_pipe(tokens[i+1]))
-// 				return (p_syntax_error(tokens[i+1]));
-// 		}
-// 		i++;
-// 	}
-// 	return (1);
-// }
-
 t_command	*parser(char *line, t_msh *msh, char **envp)
 {
 	char	**tokens;
