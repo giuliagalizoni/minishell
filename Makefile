5c163bac4c08d49cfdefc26142d25d0c9027dd17
Mario Campos <marcampo@c1b3c3.42berlin.de>
2025-05-08 17:11:21 +0200

    moved redirection fns to redirect.c

--- Makefile
+++ Makefile
@@ -37,6 +37,7 @@ MODULES := $(addprefix $(SRCDIR)/,\
 	   check_syntax.c\
 	   export_utils.c\
 	   expand_var_utils.c\
+	   redirection.c\
 	   heredoc.c\
 	   lexer_utils.c)
 
---
+++ src/redirection.c
@@ -0,0 +1,36 @@
+#include "includes/minishell.h"
+
+void	input_redirection(t_command *command)
+{
+	int	i;
+	int	file;
+
+	i = 0;
+	while(command->input_redirect[i])
+	{
+		file = open(command->input_redirect[i], O_RDONLY);
+		if (file == -1)
+			perror("Bad file descriptor");// cleanup routine here
+		dup2(file, 0);
+		close(file);
+		i++;
+	}
+}
+
+void	output_redirection(t_outfile *outfile)
+{
+	int	file;
+
+	while(outfile)
+	{
+		if (outfile->is_append)
+			file = open(outfile->filename, O_CREAT | O_WRONLY | O_APPEND, 0644);
+		else
+			file = open(outfile->filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+		if (file == -1)
+			perror("Bad file descriptor");// cleanup routine here
+		dup2(file, 1);
+		close(file);
+		outfile = outfile->next;
+	}
+}
