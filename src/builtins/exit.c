ce186a452b588485489b8ee1558908afa56c698f
Mario Campos <marcampo@c3a4c7.42berlin.de>
2025-05-09 19:03:33 +0200

    removed bye from exit

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -5,6 +5,6 @@ void	exit_shell(t_msh *msh)
 	clear_command_chain(msh->command);
 	clean_myenv(msh->myenv);
 	clear_history();
-	printf("byeeeeeee\n");
+//	printf("byeeeeeee\n");
 	exit(EXIT_SUCCESS);
 }
