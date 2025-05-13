355973fffb54921b2ff8bdc13cc2bbee3e822601
Giulia <96666886+giuliagalizoni@users.noreply.github.com>
2025-05-13 11:48:27 +0200

    Fix print literal var (#76)
    delete unused var in get path
    switch logic for printing literal var vs printing value of the var (echo "'$USER'" should print the value of the USER variable. echo '"$USER"' should print "$USER")

--- src/includes/minishell.h
+++ src/includes/minishell.h
@@ -129,7 +129,7 @@ int			safe_arr_push(char ***arr, const char *str);
 int			handle_exit_status(t_msh *msh, char ***new_tokens);
 int			process_quoted_var(char *content, t_msh *msh, char ***new_tokens);
 int			process_inner(char *content, t_msh *msh, char ***new_tokens);
-int			handle_single_quote(char *token, char ***new_tokens, size_t len);
+int			handle_double_quote(char *token, char ***new_tokens, size_t len);
 //check syntax
 int			check_invalid_syntax(char **tokens);
 
