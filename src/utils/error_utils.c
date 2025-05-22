1fcf3c9f213058842436897385b1f9e45892230d
Giulia <96666886+giuliagalizoni@users.noreply.github.com>
2025-05-22 12:27:46 +0200

    Format error msg (#90)
    * add ft_perror function
    
    * change to ft_perror function
    
    * add ft_perror function
    
    ---------
    
    Co-authored-by: Giulia Galizoni Caversan <ggalizon@c3a10c2.42berlin.de>

--- src/utils/error_utils.c
+++ src/utils/error_utils.c
@@ -21,3 +21,18 @@ void	exit_process(t_msh *msh, char *error, int exit_code)
 	if (exit_code >= 0)
 		exit(exit_code);
 }
+
+int	ft_perror(t_command *command, char *arg, int	status)
+{
+	char	*prefix;
+	char	*cmd_str;
+	char	*err_message;
+
+	prefix = "conchinha: ";
+	cmd_str = ft_strjoin(command->name, ": ");
+	err_message = ft_triplestrjoin(prefix, cmd_str, arg);
+	perror(err_message);
+	free(cmd_str);
+	free(err_message);
+	return (status);
+}
