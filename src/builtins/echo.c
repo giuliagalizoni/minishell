fed1c336b42e88259d72e1fff09d53028a2a3be1
Giulia Galizoni Caversan <ggalizon@c3a10c2.42berlin.de>
2025-05-22 11:40:27 +0200

    echo -n flag fix

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -1,30 +1,46 @@
 #include "../includes/minishell.h"
 
+static int	is_n_flag(char *arg)
+{
+	int i;
+
+	if (!arg || arg[0] != '-' || arg[1] != 'n')
+		return (0);
+	i = 2;
+	while(arg[i])
+	{
+		if (arg[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int	echo(t_command *cmd)
 {
 	int	i;
 	int	flag;
 
-	flag = 0;
+	flag = 1;
 	if (!cmd->arguments[1])
 	{
 		printf("\n");
 		return (0);
 	}
 	i = 1;
-	if (is_equal(cmd->arguments[1], "-n"))
+	while (cmd->arguments[i] && is_n_flag(cmd->arguments[i]))
 	{
+		flag = 0;
 		i++;
-		flag = 1;
 	}
-	while (cmd->arguments[i])
+	while(cmd->arguments[i])
 	{
 		printf("%s", cmd->arguments[i]);
 		if (cmd->arguments[i + 1])
 			printf(" ");
 		i++;
 	}
-	if (!flag)
+	if (flag)
 		printf("\n");
 	return (0);
 }
