9dac4a50176a1bade306a47c511b56a3eb87ece8
Giulia Galizoni <giuliagalizoni@gmail.com>
2025-05-22 23:24:46 +0200

    trying to fix export thingy

--- src/parser/process_token.c
+++ src/parser/process_token.c
@@ -3,39 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   process_token.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ggalizon <ggalizon@student.42.fr>          +#+  +:+       +#+        */
+/*   By: giuliagalizoni <giuliagalizoni@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 15:06:25 by ggalizon          #+#    #+#             */
-/*   Updated: 2025/05/20 15:08:24 by ggalizon         ###   ########.fr       */
+/*   Updated: 2025/05/22 23:22:13 by giuliagaliz      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-static int	retokenize(char ***new_tokens, char *value)
-{
-	char	**temp_tokens;
-	int		i;
-	int		success;
+// static int	retokenize(char ***new_tokens, char *value)
+// {
+// 	char	**temp_tokens;
+// 	int		i;
+// 	int		success;
 
-	temp_tokens = ft_split(value, ' ');
-	if (!temp_tokens)
-	{
-		perror("ft_split failed or returned no words");
-		return (0);
-	}
-	i = 0;
-	success = 1;
-	while (temp_tokens[i] && success)
-	{
-		success = safe_arr_push(new_tokens, temp_tokens[i]);
-		i++;
-	}
-	free_arr((void **)temp_tokens);
-	if (!success)
-		return (0);
-	return (1);
-}
+// 	temp_tokens = ft_split(value, ' ');
+// 	if (!temp_tokens)
+// 	{
+// 		perror("ft_split failed or returned no words");
+// 		return (0);
+// 	}
+// 	i = 0;
+// 	success = 1;
+// 	while (temp_tokens[i] && success)
+// 	{
+// 		success = safe_arr_push(new_tokens, temp_tokens[i]);
+// 		i++;
+// 	}
+// 	free_arr((void **)temp_tokens);
+// 	if (!success)
+// 		return (0);
+// 	return (1);
+// }
 
 static int	handle_single_quote(char *token, char ***new_tokens)
 {
@@ -60,18 +60,65 @@ static int	handle_unquoted_var(char *token, t_msh *msh, char ***new_tokens)
 {
 	char	*key;
 	char	*value;
+	char	*remainder;
+	char	*expanded_token;
+	int		var_length;
 	int		success;
 
-	success = 1;
-	key = ft_substr(token, 1, ft_strlen(token) - 1);
+	// Determine where the variable ends (e.g., `$VARtest`)
+	var_length = 1; // Start after `$`
+	while (token[var_length] && ft_isalnum(token[var_length]))
+		var_length++;
+
+	// Extract the variable key
+	key = ft_substr(token, 1, var_length - 1); // Skip `$` and take only the key part
 	if (!key)
 		return (perror("failed creating key substring"), 0);
+
+	// Extract the remainder (if any, e.g., `test` in `$VARtest`)
+	remainder = ft_strdup(token + var_length);
+	if (!remainder)
+		return (free(key), perror("failed duplicating remainder"), 0);
+
+	// Get the value of the variable
 	value = get_var_value(msh->myenv, key);
 	free(key);
+
+	// Combine expanded value and remainder
 	if (value)
-		success = retokenize(new_tokens, value);
+		expanded_token = ft_strjoin(value, remainder);
+	else
+		expanded_token = ft_strdup(remainder);
+
+	free(remainder);
+	if (!expanded_token)
+		return (perror("failed combining expanded token"), 0);
+
+	// Push the expanded token to new_tokens
+	success = safe_arr_push(new_tokens, expanded_token);
+	free(expanded_token);
+
 	return (success);
 }
+
+
+
+// static int	handle_unquoted_var(char *token, t_msh *msh, char ***new_tokens)
+// {
+// 	char	*key;
+// 	char	*value;
+// 	int		success;
+
+// 	success = 1;
+// 	key = ft_substr(token, 1, ft_strlen(token) - 1);
+// 	if (!key)
+// 		return (perror("failed creating key substring"), 0);
+// 	value = get_var_value(msh->myenv, key);
+// 	free(key);
+// 	if (value)
+// 		success = retokenize(new_tokens, value);
+// 	return (success);
+// }
 
 static int	handle_mixed_token(char *token, char ***new_tokens, t_msh *msh)
 {
