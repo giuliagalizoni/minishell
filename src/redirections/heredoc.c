1b9822567bde6f3382abb9444df18d03f3b401a5
Giulia <96666886+giuliagalizoni@users.noreply.github.com>
2025-05-27 17:26:22 +0200

    heredoc functions split into smaller (#115)
    Co-authored-by: Giulia Galizoni Caversan <ggalizon@c3a10c2.42berlin.de>

--- src/redirections/heredoc.c
+++ src/redirections/heredoc.c
@@ -1,46 +1,63 @@
 #include "../includes/minishell.h"
 
-static void	write_line(int	*pipe_fd, char *expanded_line)
+static void	write_to_heredoc_pipe(int write_fd, char *expanded_line)
 {
-	write(pipe_fd[1], expanded_line, ft_strlen(expanded_line));
-	write(pipe_fd[1], "\n", 1);
+	write(write_fd, expanded_line, ft_strlen(expanded_line));
+	write(write_fd, "\n", 1);
 	free(expanded_line);
+}
+
+static int	manage_heredoc_line(char *line, t_command *command,
+						t_msh *msh, int write_fd)
+{
+	char	*expanded_line;
+
+	if (g_signal_code == SIGINT)
+	{
+		msh->exit_status = 130;
+		if (line)
+			free(line);
+		return (-1);
+	}
+	if (!line)
+	{
+		ft_perror(command->name, NULL, 1,
+			"warning: here-document delimited by end-of-file");
+		return (0);
+	}
+	if (is_equal(line, command->heredoc_delimiter))
+	{
+		free(line);
+		return (0);
+	}
+	expanded_line = expand_inline(line, msh);
+	free(line);
+	if (expanded_line)
+		write_to_heredoc_pipe(write_fd, expanded_line);
+	return (1);
 }
 
 int	handle_heredoc(t_command *command, t_msh *msh)
 {
 	int		pipe_fd[2];
-	char	*line;
-	char	*expanded_line;
+	char	*current_line;
+	int		line_status;
 
 	if (pipe(pipe_fd) == -1)
 		error_cleanup(msh);
 	command->heredoc_fd = pipe_fd[0];
 	while (1)
 	{
-		line = readline("> ");
-		if (g_signal_code == SIGINT)
+		current_line = readline("> ");
+		line_status = manage_heredoc_line(current_line, command,
+				msh, pipe_fd[1]);
+		if (line_status == -1)
 		{
-			msh->exit_status = 130;
-			if (line)
-				free(line);
 			close(pipe_fd[1]);
-			return (0); // Indicate interruption
+			return (0);
 		}
-		if (!line)
-		{
-			ft_perror(command->name, NULL, 1, "warning: here-document delimited by end-of-file");
+		if (line_status == 0)
 			break ;
-		}
-		if (is_equal(line, command->heredoc_delimiter))
-		{
-			free(line);
-			break ;
-		}
-		expanded_line = expand_inline(line, msh);
-		free(line);
-		if (expanded_line)
-			write_line(pipe_fd, expanded_line);
 	}
 	close(pipe_fd[1]);
 	return (1);
