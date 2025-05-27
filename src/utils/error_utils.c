2eb98c9b74e8a938cd1f4ba5b24a3d37d2d2ef4a
Giulia Galizoni Caversan <ggalizon@c3a10c2.42berlin.de>
2025-05-27 12:42:41 +0200

    delete return_error function

--- src/utils/error_utils.c
+++ src/utils/error_utils.c
@@ -12,14 +12,6 @@ void	export_error(char *arg)
 	write(2, "\': not a valid identifier\n", 26);
 }
 
-// I'll make this function better soon
-int	return_error(char *error_msg)
-{
-	if (error_msg)
-		perror(error_msg);
-	return (0);
-}
-
 void	exit_process(t_msh *msh, t_command *command, char *arg,
 			char *err_msg, int status)
 {
@@ -28,48 +20,6 @@ void	exit_process(t_msh *msh, t_command *command, char *arg,
 	cleanup(msh);
 	exit(status);
 }
-
-// int	ft_perror(t_command *command, char *arg, int status, char *err_msg)
-// {
-// 	char	*tmp;
-// 	char	*err_str;
-
-// 	err_str = ft_strdup("conchinha: ");
-// 	if (command->name)
-// 	{
-// 		tmp = ft_strjoin(err_str, command->name);
-// 		free(err_str);
-// 		err_str = tmp;
-// 	}
-// 	if (arg && !command->name)
-// 	{
-// 		tmp = ft_strjoin(err_str, arg);
-// 		free(err_str);
-// 		err_str = tmp;
-// 	}
-// 	else
-// 	{
-// 		if (arg)
-// 		{
-// 			tmp = ft_triplestrjoin(err_str, ": ", arg);
-// 			free(err_str);
-// 			err_str = tmp;
-// 		}
-// 	}
-// 	if (err_msg)
-// 	{
-// 		tmp = ft_triplestrjoin(err_str, ": ", err_msg);
-// 		free(err_str);
-// 		err_str = tmp;
-// 		ft_putstr_fd(err_str, STDERR_FILENO);
-// 		ft_putchar_fd('\n', STDERR_FILENO);
-// 		free(err_str);
-// 		return (status);
-// 	}
-// 	perror(err_str);
-// 	free(err_str);
-// 	return (status);
-// }
 
 int	invalid_option_error(t_command *cmd, char *arg, int status)
 {
