fdc5cbcc3508e965fb42dfa6263f5ce126abe4ac
Giulia Galizoni <giuliagalizoni@gmail.com>
2025-05-28 16:31:53 +0200

    move child_process to separate file and break it into smaller helpers

--- src/executer/child_process.c
+++ src/executer/child_process.c
@@ -1,49 +1,49 @@
 #include "../includes/minishell.h"
 
 static void	handle_child_pipe_io(t_msh *msh, t_command *cmd,
-    int prev_p_fd, int *p_fds)
+	int prev_p_fd, int *p_fds)
 {
-    if (prev_p_fd != STDIN_FILENO)
-    {
-        if (dup2(prev_p_fd, STDIN_FILENO) == -1)
-            exit_process(msh, cmd, NULL, "dup2 for stdin failed");
-        close(prev_p_fd);
-    }
-    if (cmd->index < msh->num_cmds - 1)
-    {
-        close(p_fds[0]);
-        if (dup2(p_fds[1], STDOUT_FILENO) == -1)
-            exit_process(msh, cmd, NULL, "dup2 for stdout failed");
-        close(p_fds[1]);
-    }
+	if (prev_p_fd != STDIN_FILENO)
+	{
+		if (dup2(prev_p_fd, STDIN_FILENO) == -1)
+			exit_process(msh, cmd, NULL, "dup2 for stdin failed");
+		close(prev_p_fd);
+	}
+	if (cmd->index < msh->num_cmds - 1)
+	{
+		close(p_fds[0]);
+		if (dup2(p_fds[1], STDOUT_FILENO) == -1)
+			exit_process(msh, cmd, NULL, "dup2 for stdout failed");
+		close(p_fds[1]);
+	}
 }
 
 static void	handle_child_file_redirections(t_msh *msh, t_command *cmd)
 {
-    char	*input_target;
+	char	*input_target;
 
-    input_target = NULL;
-    if (cmd->heredoc_is_final)
-    {
-        if (cmd->is_heredoc && cmd->heredoc_fd != -1)
-        {
-            if (dup2(cmd->heredoc_fd, STDIN_FILENO) == -1)
-                exit_process(msh, cmd, NULL, "dup2 for heredoc failed");
-            close(cmd->heredoc_fd);
-        }
-    }
-    else if (cmd->input_redirect && cmd->input_redirect[0] != NULL)
-    {
-        if (cmd->input_redirect[0]) // Get a target for error message if any
-            input_target = cmd->input_redirect[0];
-        if (!input_redirection(cmd))
-            exit_process(msh, cmd, input_target, NULL);
-    }
-    if (cmd->outfile)
-    {
-        if (!output_redirection(cmd->outfile))
-            exit_process(msh, cmd, NULL, "output redirection failed");
-    }
+	input_target = NULL;
+	if (cmd->heredoc_is_final)
+	{
+		if (cmd->is_heredoc && cmd->heredoc_fd != -1)
+		{
+			if (dup2(cmd->heredoc_fd, STDIN_FILENO) == -1)
+				exit_process(msh, cmd, NULL, "dup2 for heredoc failed");
+			close(cmd->heredoc_fd);
+		}
+	}
+	else if (cmd->input_redirect && cmd->input_redirect[0] != NULL)
+	{
+		if (cmd->input_redirect[0])
+			input_target = cmd->input_redirect[0];
+		if (!input_redirection(cmd))
+			exit_process(msh, cmd, input_target, NULL);
+	}
+	if (cmd->outfile)
+	{
+		if (!output_redirection(cmd->outfile))
+			exit_process(msh, cmd, NULL, "output redirection failed");
+	}
 }
 
 static void	validate_command_for_execution(t_msh *msh, t_command *cmd)
@@ -56,19 +56,20 @@ static void	validate_command_for_execution(t_msh *msh, t_command *cmd)
 	if (!cmd->path)
 	{
 		msh->exit_status = 127;
-		exit_process(msh, cmd, cmd->name, "command not found");
+		exit_process(msh, cmd, NULL, "command not found");
 	}
 	if (is_directory(cmd->path))
 	{
 		msh->exit_status = 126;
-		exit_process(msh, cmd, cmd->path, "Is a directory");
+		exit_process(msh, cmd, NULL, "Is a directory");
 	}
 	if (access(cmd->path, X_OK) == -1)
 	{
 		msh->exit_status = 126;
-		exit_process(msh, cmd, cmd->path, "Permission denied");
+		exit_process(msh, cmd, NULL, "Permission denied");
 	}
 }
+
 static void	execute_command_in_child(t_msh *msh, t_command *cmd)
 {
 	char	**envp;
@@ -89,77 +90,7 @@ void	child_process(t_msh *msh, t_command *command,
 	int prev_pipe_read_fd, int *fd)
 {
 	msh->exit_status = 1;
-
 	handle_child_pipe_io(msh, command, prev_pipe_read_fd, fd);
 	handle_child_file_redirections(msh, command);
 	execute_command_in_child(msh, command);
 }
-
-// void	child_process(t_msh *msh, t_command *command,
-// 	int prev_pipe_read_fd, int *fd)
-// {
-// 	char	**envp;
-
-// 	msh->exit_status = 1;
-// 	if (prev_pipe_read_fd != STDIN_FILENO)
-// 	{
-// 		if (dup2(prev_pipe_read_fd, STDIN_FILENO) == -1)
-// 			exit_process(msh, command, NULL, "dup 2 fail for stdin redirection");
-// 		close(prev_pipe_read_fd);
-// 	}
-// 	if (command->index < msh->num_cmds - 1)
-// 	{
-// 		close(fd[0]);
-// 		if (dup2(fd[1], STDOUT_FILENO) == -1)
-// 			exit_process(msh, command, NULL,  "dup 2 fail for stdout redirection");
-// 		close(fd[1]);
-// 	}
-// 	if (command->heredoc_is_final)
-// 	{
-// 		if (command->is_heredoc && command->heredoc_fd != -1)
-// 		{
-// 			if (dup2(command->heredoc_fd, STDIN_FILENO) == -1)
-// 				exit_process(msh, command, NULL, "dup 2 fail for heredoc redirection");
-// 			close(command->heredoc_fd);
-// 		}
-// 	}
-// 	else if (command->input_redirect && command->input_redirect[0] != NULL)
-// 		if (!input_redirection(command))
-// 			exit_process(msh, command, command->input_redirect[0], NULL);
-// 	if (command->outfile)
-// 		if (!output_redirection(command->outfile))
-// 			exit_process(msh, command, NULL, "output redirection failed");
-// 	if (is_builtin(command->name))
-// 		child_builtin(msh, command);
-// 	if (command->path && is_directory(command->path))
-// 	{
-// 		msh->exit_status = 126;
-// 		exit_process(msh, command, NULL, "Is a directory");
-// 	}
-// 	if (command->path && access(command->path, X_OK))
-// 	{
-// 		msh->exit_status = 126;
-// 		exit_process(msh, command, NULL, "Permission denied");
-// 	}
-// 	else
-// 	{
-// 		if (!command->name)
-// 		{
-// 			msh->exit_status = 0;
-// 			exit_process(msh, command, NULL, NULL);
-// 		}
-// 		if (!command->path)
-// 		{
-// 			msh->exit_status = 127;
-// 			exit_process(msh, command, NULL, "command not found");
-// 		}
-// 		envp = myenv_to_envp(msh->myenv);
-// 		if (execve(command->path, command->arguments, envp) == -1)
-// 		{
-// 			free_arr((void **)envp);
-// 			msh->exit_status = 127;
-// 			exit_process(msh, command, NULL, "command not found");
-// 		}
-// 		exit(EXIT_SUCCESS);
-// 	}
-// }
