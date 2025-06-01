f5f7a155b106db7c369b4436e5dc3d390e57e76c
Mario Campos <marcampo@c3a4c2.42berlin.de>
2025-06-01 20:19:49 +0200

    cd without params fix for segfaults when PWD or HOME or OLDPWD are unset

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -19,10 +19,17 @@ int	cd(t_command *command, t_vars *myenv)
 	getcwd(pwd, PATH_MAX);
 	if (command->arguments[1] == NULL)
 	{
-		update_var_value(find_var(myenv, "OLDPWD"), (const char *)pwd);
-		if (chdir(find_var(myenv, "HOME")->value) != 0)
-			return (ft_perror(command->name, find_var(myenv, "HOME")->value, 1, NULL));
-		update_var_value(find_var(myenv, "PWD"), find_var(myenv, "HOME")->value);
+		if (find_var(myenv, "HOME"))
+		{
+			if (find_var(myenv, "OLDPWD"))
+				update_var_value(find_var(myenv, "OLDPWD"), (const char *)pwd);
+			if (chdir(find_var(myenv, "HOME")->value) != 0)
+				return (ft_perror(command->name, find_var(myenv, "HOME")->value, 1, NULL));
+			if (find_var(myenv, "PWD"))
+				update_var_value(find_var(myenv, "PWD"), find_var(myenv, "HOME")->value);
+		}
+		else
+			return (ft_perror(command->name, NULL, 1, "HOME not set"));
 	}
 	else if (command->arguments[2])
 		return (ft_perror(command->name, NULL, 1, "too many arguments"));
@@ -35,9 +42,11 @@ int	cd(t_command *command, t_vars *myenv)
 			return (ft_perror(command->name, command->arguments[1], 1, NULL));
 		else
 		{
-			update_var_value(find_var(myenv, "OLDPWD"), (const char *)pwd);
+			if (find_var(myenv, "OLDPWD"))
+				update_var_value(find_var(myenv, "OLDPWD"), (const char *)pwd);
 			getcwd(pwd, PATH_MAX);
-			update_var_value(find_var(myenv, "PWD"), (const char *)pwd);
+			if (find_var(myenv, "PWD"))
+				update_var_value(find_var(myenv, "PWD"), (const char *)pwd);
 		}
 	}
 	return (0);
